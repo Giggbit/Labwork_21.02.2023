abe235eb881356fbafb022d393e824928f652ccc
Giggbit <danya19981017@gmail.com>
2023-02-21 12:51:30 +0100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labwork_21._02._2023
+{
+    internal class Program
+    {
+        delegate bool MyDel(int x);
+        delegate int Square(int x);
+        delegate void ProgramDay(int x);
+        delegate void Massive(int[] mas);
+
+        static void Main(string[] args)
+        {
+            #region ex1
+            /* MyDel del = delegate (int a)
+             {
+                 return a % 2 == 0;
+             };
+             Console.WriteLine($"{del(11)}");*/
+            #endregion
+
+            #region ex2
+            /* Square square = delegate (int a)
+             {
+                 return a * a;
+             };
+             Console.WriteLine($"Square num = {square(5)}");*/
+            #endregion
+
+            #region ex3
+            /*Square SquareInt = i => i * i * i;
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = SquareInt(number);
+            Console.WriteLine("Квадрат числа {0} равен {1}", number, result);*/
+            #endregion
+
+            #region ex4
+            /*ProgramDay pDay = (int d) => {
+                if(d == 256) {
+                    Console.WriteLine("It's day of programmer!");
+                }
+                else { Console.WriteLine("It's not a day of programmer"); }
+            };
+
+            Console.Write("Enter day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            pDay(day);*/
+            #endregion
+
+            #region ex5
+            /*int[] mas = new int[] { 1, 2, 3, 4, 5, 6 };
+            int max = 0;
+
+            Massive Max = (int[] arr) => {
+               foreach(int a in arr) { 
+                    if(max < a) { max = a; } 
+               }
+            };
+
+            Max(mas);
+            Console.WriteLine($"Max = {max}");*/
+            #endregion
+
+            #region ex6
+            /*int[] mas = new int[] { 1, 2, 3, 4, 5, 6 };
+            int min = mas[0];
+
+            Massive Min = (int[] arr) => {
+               foreach(int a in arr) { 
+                    if(min > a) { min = a; } 
+               }
+            };
+
+            Min(mas);
+            Console.WriteLine($"Min = {min}");*/
+            #endregion
+
+            #region ex7
+            /*int[] mas = new int[] { 11, 56, 23, 89, 21, 33, 4, 8 };
+
+            Massive Nechet = (int[] arr) => {
+                foreach (int a in arr){
+                    if (a % 2 != 0) {
+                        Console.WriteLine(a);
+                    }
+                }
+            };
+
+            Nechet(mas);*/
+            #endregion
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Labwork 21.02.2023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Labwork 21.02.2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("19c4ac55-7fcd-4ffe-bbba-691e62e09dc3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
